a375102e788f6999d61b2bc634fb5fc7c8844164
malexsan <alexandru.munt@gmail.com>
2022-08-17 14:32:00 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <h1 className="text-center">Welcome to the Trivia Challenge!</h1>
+    </div>
+  )
+}
+
+export default App
