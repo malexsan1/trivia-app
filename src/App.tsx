37e11477fce36ab80e872bae6a0159f6b0eb1390
malexsan <alexandru.munt@gmail.com>
2022-08-17 14:58:49 +0300

    add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="h-full flex items-center justify-center">
       <h1 className="text-center">Welcome to the Trivia Challenge!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
